253c2f7519b81992f9c5e9733cc52c07f9f39add
Deny Prasetyo <jasoet87@gmail.com>
2014-06-19 20:14:50 +0700

    Add Spring MVC

---
+++ src/main/java/id/ac/uad/tot/controller/HomeController.java
@@ -0,0 +1,31 @@
+package id.ac.uad.tot.controller;
+
+/**
+ * Created by Deny Prasetyo,S.T
+ * Java(Script) Developer and Trainer
+ * Co-Founder | 2ndStack Studio
+ * jasoet87@gmail.com
+ * <p/>
+ * http://github.com/jasoet
+ * http://bitbucket.com/jasoet
+ * <p/>
+ * [at]jasoet
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home(ModelMap modelMap) {
+
+        return "home";
+    }
+
+
+}
+
---
+++ src/main/webapp/WEB-INF/jsp/home.jsp
@@ -0,0 +1,6 @@
+<html>
+<head><title>Hore Bisa</title></head>
+<body>
+<div>HOREEEE</div>
+</body>
+</html>
